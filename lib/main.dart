2f5a5f88d935ec5fb00dcbe800f5412ecb26df40
olonyl <o.rocha@contolloconsulting.com>
2020-04-15 09:22:59 -0600

    flutter widgets

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app_widgets/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+      title: "Simples layout",
+      home: new Home(),
+    ));
